885f7484e0b87a61f586ce5803d676b69c19edb7
Joseph Chalabi <chalabi.joseph@gmail.com>
2025-05-15 02:35:55 -0700

    add berkley mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,44 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import "./prism.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/nav-bar";
 import PageTransition from "@/components/page-transition";
+import localFont from "next/font/local";
 
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Joseph Chalabi | Portfolio",
   description: "Portfolio website showcasing my projects and skills",
 };
+
+const berkleyMono = localFont({
+  src: [
+    {
+      path: "../public/fonts/berkley-mono/BerkeleyMono-Regular.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/berkley-mono/BerkeleyMono-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/berkley-mono/BerkeleyMono-Oblique.woff2",
+      weight: "500",
+      style: "oblique",
+    },
+    {
+      path: "../public/fonts/berkley-mono/BerkeleyMono-Bold-Oblique.woff2",
+      weight: "700",
+      style: "oblique",
+    },
+  ],
+  variable: "--font-berkley-mono",
+});
 
 export default function RootLayout({
   children,
@@ -30,9 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${berkleyMono.className}  antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
