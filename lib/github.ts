c501e6ba546c124340eeac00c4532713eac5e967
Joseph Chalabi <chalabi.joseph@gmail.com>
2025-11-05 16:12:20 -0700

    add caddy dynamic timeouts module

--- lib/github.ts
+++ lib/github.ts
@@ -56,6 +56,7 @@ const projectConfig: ProjectConfig = {
     "caddy-blockchain-health",
     "prop-voter",
     "caddy-stateful-jwt-auth",
+    "caddy-dynamic-timeouts",
     "twap-tax"
   ],
   featured: [
@@ -64,7 +65,7 @@ const projectConfig: ProjectConfig = {
     "manifest-app",
     "skip-go-fast-solver-ui",
     "quicksilver",
-     "caddy-blockchain-health",
+    "caddy-blockchain-health",
     "LFP"
   ],
   customData: {
